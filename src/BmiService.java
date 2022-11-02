1ab924b6933a88c62799d66bc72ab522001e9aae
Dima Vasyutin <Vasyutin_dima12@mail.ru>
2022-11-02 11:55:16 +0300

    Add BMICalculator

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public float calculate(float i, float a){
+        float b = i / (a * a);
+        return b;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        float mass = 74F;
+        float hight = 1.72F;
+        float bmi = service.calculate(mass,hight);
+        System.out.println(bmi);
+    }
+}
