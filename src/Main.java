d083d73865439bf97551ca1006ba45a0f324c3f3
DimaVasyutin <115107603+DimaVasyutin@users.noreply.github.com>
2022-11-05 13:27:22 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BmiService service = new BmiService();
         float mass = 74F;
         float hight = 1.72F;
-        float bmi = service.calculate(mass,hight);
+        float bmi = service.calculate(mass, hight);
         System.out.println(bmi);
     }
 }
